e1db25616d18e4e8ea0387ceeffb0f958e2dd3a1
Gopvora14 <gopvora5@gmail.com>
2026-09-24 17:14:11 +0530

    new  validation  changes

--- validation.py
+++ validation.py
@@ -1,11 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
-class  User (BaseModel):
-    id: int
-    Name: str = "Madhav"
+# Create User class witht the help of BaseModel for Validation
+class User(BaseModel):
+    id: int  # Select the data type of class member
+    name: str
     sign_ts: str | None = None
-    isActive:bool
-    email:
-    
-    objUser = User ( id=30 , Name="Madhav")
-    print (objUser)
+    isActive: bool
+    email: EmailStr
+
+
+# Create the Object using User Class
+objUser = User(id=123, name="Gop", isActive=False, email="gop2026@gmail.com")
+print(objUser)
